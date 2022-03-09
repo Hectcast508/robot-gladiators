219e323b3218d33ffb54596d8e31afac92da7e5c
Capwonderbread <hectcast10@yahoo.com>
2022-03-09 08:18:04 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,12 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
@@ -26,14 +32,16 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -47,12 +55,15 @@ var fight = function(enemyName) {
   
         // leave while() loop since enemy is dead
         break;
-      } else {
+      }
+      else {
         window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -64,9 +75,10 @@ var fight = function(enemyName) {
         break;
       } else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
-      }  
-  }  
+      }
+  } 
 };
+
 
 var startGame = function() {
   playerHealth = 100;
@@ -75,10 +87,10 @@ var startGame = function() {
 
   for(var i = 0; i < enemyNames.length; i++) {
       if (playerHealth > 0) {
-          window.alert('Welcome to Robot Gladiators! Round ' + (i+i));
+          window.alert('Welcome to Robot Gladiators! Round ' + (i +1));
       
           var pickedEnemyName = enemyNames[i];
-          enemyHealth = 50;
+          enemyHealth = randomNumber(40, 60);
           fight(pickedEnemyName);
 
           if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -102,6 +114,8 @@ var startGame = function() {
 };
 
 var endGame = function() {
+  window.alert("The game has now ended. Lets see how you did!");
+
   if (playerHealth > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
   }
@@ -122,7 +136,7 @@ var endGame = function() {
 
 var shop = function() {
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to maske a choice."
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: "REFILL", "UPGRADE", or "LEAVE" to maske a choice.'
   );
 
   switch (shopOptionPrompt) {
@@ -167,5 +181,7 @@ var shop = function() {
       break;
   }
 };
+
+
 
 startGame();
